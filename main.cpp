7740b63977524dc4a95f33945f5434624396513d
jparep <parep@hawaii.edu>
2024-12-18 18:33:25 -0700

    Ensured author as const

--- main.cpp
+++ main.cpp
@@ -10,7 +10,13 @@ public:
     std::string author;
     bool is_borrowed;
 
-    // Constructor 
-    Book(int id, const std::string& title), std::string& author, bool is_borrowed=false)
-        : id(id), tittle(title), author(author), is_borrowed(is_borrow){}
-    {
+    // Constructor
+    Book(int id, const std::string& title, const std::string& author, bool is_borrowed=false)
+        : id(id), title(title), author(author), is_borrowed(is_borrowed) {}
+
+    // Display Method
+    void display() const {
+        std::cout << "ID: " << id << " | Title: " << title << " | Author: " << author
+                  << " | Status: " << (is_borrowed ? "Borrowed" : "Available") << '\n';
+    }
+};
