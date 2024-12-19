ed54cd925d305cb43ad9554297d24d6da40aa874
jparep <parep@hawaii.edu>
2024-12-18 18:22:43 -0700

    Class book created and public added

--- main.cpp
+++ main.cpp
@@ -2,3 +2,12 @@
 #include <fstream>
 #include <vector>
 #include <string>
+
+class Book {
+public:
+    int id;
+    std::string title;
+    std::string author;
+    bool is_borrowed;
+    // Constructor  
+{
