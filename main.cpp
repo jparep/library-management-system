bc8afec4de771cf4d8c1f340afa70a2f9a648fb5
jparep <parep@hawaii.edu>
2024-12-25 12:17:27 -0700

    Optimized Book classs and class methods

--- main.cpp
+++ main.cpp
@@ -9,21 +9,18 @@ using namespace std;
 class Book {
 public:
     int id;
-    std::string title;
-    std::string author;
+    string title;
+    string author;
     bool is_borrowed;
 
-    // Constructor
-    Book(int id, const std::string& title, const std::string& author, bool is_borrowed=false)
+    Book(int id, const string& title, const string& author, bool is_borrowed = false)
         : id(id), title(title), author(author), is_borrowed(is_borrowed) {}
 
-    // Display Method
     void display() const {
-        std::cout << "ID: " << id << " | Title: " << title << " | Author: " << author
-                  << " | Status: " << (is_borrowed ? "Borrowed" : "Available") << '\n';
+        cout << "ID: " << id << " | Title: " << title << " | Author: " << author
+             << " | Status: " << (is_borrowed ? "Borrowed" : "Available") << '\n';
     }
 };
-
 
 class Library {
 private:
